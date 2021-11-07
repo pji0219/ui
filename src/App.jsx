ec8be8e4c0cf520024c728bce72c1de44fad7cae
pji0219 <pji219@naver.com>
2021-11-07 23:46:18 +0900

    스켈레톤 구현중

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,80 @@
 import React from "react";
+import styled from '@emotion/styled';
+import Skeleton from "./components/Skeleton";
+
+const Base = styled.div`
+  display: grid;
+  width: 100%
+  grid-template-columns: repeat(5, 1fr);
+  column-gap: 12px;
+  row-gap: 24px
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  box-shadow: rgb(0 0 0 / 4%) 0px 4px 16px 0px;
+  border-radius: 4px;
+`;
+
+const ImageWrapper = styled.div`
+  width: 100%;
+`;
+
+const Image = styled.img`
+  width: 500px;
+  height: 500px;
+  object-fit: cover;
+`;
+
+const Info = styled.div`
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 0%;
+`;
+
+const Title = styled.h4`
+  margin: 0;
+  padding: 0;
+  font-size: 24px;
+`;
+
+const Description = styled.p`
+  margin: 8px 0 0 0;
+  padding: 0;
+  font-size: 16px;
+`;
+
+const Placeholder = () => (
+  <Container>
+    
+  </Container>
+)
+
+const Item = () => {
+  return (
+    <Container>
+      <ImageWrapper>
+        <Image src="http://www.animaltogether.com/news/photo/201908/403_901_916.png" alt="잠자는 고양이" />
+      </ImageWrapper>
+      <Info>
+        <Title>Cat taking a nap</Title>
+        <Description>zzzzzzzzzz</Description>
+      </Info>
+    </Container>
+  )
+}
 
 function App() {
   return (
-    <div>
-      Hello skeleton
-    </div>
+    <Base>
+      {
+        Array.from({ length: 25 }).map((_, idx) => (
+          <Item key={idx} />
+        ))
+      }
+    </Base>
   );
 }
 
--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import styled from '@emotion/styled';
-import { keyframes } from '@emotion/react';
+import { keyframes, css } from '@emotion/react';
 
-const Base = styled.span<Props>`
+const pulseKeyframe = keyframes`
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0.4%;
+  }
+  100% {
+    opacity: 1;
+  }
+`;
+
+const pulseAnimation = css`
+  animation: ${pulseKeyframe} 1.5s ease-in-out infinite;
+`;
+
+const Base = styled.span`
   ${({ color }) => color && `background-color: ${color}`};
   ${({ rounded }) => rounded && 'border-radius: 8px'};
   ${({ circle }) => circle && 'border-radius: 50%'};
   ${({ width, height }) => (width || height) && 'display: block'};
+  ${({ animation }) => animation && pulseAnimation };
+  width: ${({ width, unit }) => width && unit && `${width}${unit}`};
+  height: ${({ height, unit }) => height && unit && `${height}${unit}`};
 `;
 
 const Content = styled.span`
   opacity: 0;
 `;
 
-function Skeleton() {
+function Skeleton({ count }) {
+  // count 5 => '-----'
+  const content = useMemo(() => [...Array({ length: count })].map(() => '-').join(''), [count]);
+
   return (
     <Base>
-      <Content></Content>
+      <Content>{content}</Content>
     </Base>
   );
 }
